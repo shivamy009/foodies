2ed2dcafffc590dc8c66c0b2a43ee933956360ce
shivamy009 <shivam0342005@gmail.com>
2024-03-19 23:41:40 +0530

    Price filter change

--- my-app/src/Components/Prices.js
+++ my-app/src/Components/Prices.js
@@ -1,33 +1,33 @@
 export const Prices =[
     {
         _id:0,
-        name:"$0 to $19",
-        array:[0,19]
+        name:"$0 to $39",
+        array:[0,39]
     },
     {
         _id:1,
-        name:"$20 to $39",
-        array:[20,39]
+        name:"$40 to $69",
+        array:[40,69]
     },
     {
         _id:2,
-        name:"$40 to $59",
-        array:[40,59]
+        name:"$70 to $99",
+        array:[70,99]
     },
     {
         _id:3,
-        name:"$60 to $79",
-        array:[60,79]
+        name:"$100 to $119",
+        array:[100,119]
     },
     {
         _id:4,
-        name:"$80 to $99",
-        array:[80,99]
+        name:"$120 to $139",
+        array:[120,139]
     },
     {
         _id:5,
-        name:"$100 or more",
-        array:[100,9999]
+        name:"$139 or more",
+        array:[139,9999]
     },
 
 ]
